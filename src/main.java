cf2dbc5f8a7ec7a97ab4be36535f479102b05450
Việt Anh <aoracklack97@gmail.com>
2022-07-18 18:43:59 +0700

    hoang 123

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.printf("chao hoang ");
+        System.out.printf("chao hoang ");
+
     }
 }
